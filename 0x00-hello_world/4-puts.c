5f8261052b8a9bf09eb792eec708f05c62ffbdeb
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-08 06:13:04 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,19 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
- * *main -prints
- * use function puts
- * Return :0 if exited properly
- * */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main( void)
 
-{
+{puts("\"Programming is like building a multilingual puzzle");
 
+	 return (0);
 
-
-	puts("\"Programming is like building a multilingual puzzzle");
-
-		return 0;
-
-}
+	  }
