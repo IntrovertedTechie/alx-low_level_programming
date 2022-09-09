abe0f45f0fbe7a342c85a3b7a54a4fc69dc79f80
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-09 12:17:46 -0700

    sigle digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/*header*/
+/* mainfubction */
+int main(void)
+{
+int alp;
+for(alp=48;alp<58;alp++)
+{
+putchar(alp);
+}
+putchar(10);
+return(0);
+}
