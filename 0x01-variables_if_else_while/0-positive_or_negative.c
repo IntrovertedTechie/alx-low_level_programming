d1b04f7c768e9238f305e7ac9ff065571a3e05cc
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-10 05:54:16 -0700

    hot fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,36 @@
-/*
- * Doc :Zero,Positive Negative number indicator
- * Author :Adio Adebayo
- */
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 /**
  * main - prints a random number
- * and indicates 
- * whether it is zero
- * positive or negative
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
      int n;
-     
      srand(time(0));
      
-     n=rand()-RAND_MAX / 2;
+     n = rand() - RAND_MAX / 2;
 
-    if(n > 0){ 
+    if (n > 0)
+{ 
 
-    printf("%d is positive \n",n); 
-    }else if(n <0){
+   printf("%dis positive\n",n ); 
 
-    printf("%d is negative\n",n);  
-    }else{
+}  else if (n <0 )
 
-    printf("%d is zero\n",n);
-    }
+{
+   printf("%d is negative\n",n);  
+   
+}  else
+
+{
+
+   printf("%d is zero\n",n);
+  
+}
     return (0);
 }
