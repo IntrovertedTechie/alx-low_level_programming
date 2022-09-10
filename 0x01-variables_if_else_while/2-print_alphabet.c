e35ba4ceff97aed4356c8c4c618ec9758f690ae2
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-09 19:39:34 -0700

    hot fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include<time.h>
-/*more headers can go here */
-/*betty style doc for functmain goes there */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 ( Success)
+ */
 int main(void)
 {
 int alp;
-for (alp=97;alp<=122; alp++)
+for (alp = 97; alp <= 122; alp++)
 {
 putchar(alp);
 }
