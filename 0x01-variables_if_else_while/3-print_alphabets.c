0840e1130d2780d2353c709f868c78a189f0b43a
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-09 19:35:05 -0700

    hot fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int alp;
 int al;
-for(alp=97;alp<=122;alp++)
+for (alp = 97; alp <= 122; alp++)
 {
 putchar(alp);
 }
-for(al=65;al<=90;al++)
+for (al = 65; al <= 90; al++)
 {
 putchar(al);
 }
 putchar(10);
-return(0);
+return (0);
 }
