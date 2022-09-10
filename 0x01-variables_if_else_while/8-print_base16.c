dc67058a9e1b60639e49a3db50566666fd7a19d5
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-09 18:24:48 -0700

    hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 #include <time.h>
-
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-
+/**
+ *  main  - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 char ch;
