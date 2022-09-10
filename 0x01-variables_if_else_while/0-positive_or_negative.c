de33d5c5c0d3768f5abab794ee664d94099c6c38
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-10 07:35:12 -0700

    hot fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive\n", n); 
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%d is negative\n", n);  
+printf("%d is negative\n", n);
 }
 else
 {
@@ -27,4 +27,3 @@ printf("%d is zero\n", n);
 }
 return (0);
 }
-autocmd BufWritePre *.c :%s/\s\+$//e
