ea6ffaa19de5248878e82825f83a77792891b0b2
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-11 00:13:27 -0700

    2 digit combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/** 
+ * main - print combination of two digit
+ *
+ * Retuen: Always 0 (Success)
+ */
+int main(void)
+{
+int alp;
+int alpha;
+for (alp = 48 ; alp <= 57 ; alp++)
+{
+for (alpha = 49; alpha <= 57 ; alpha++)
+if (alpha > alp)
+{		
+putchar(alp);
+putchar(alpha);
+if (alp != 56 || alpha != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar(10);
+return (0);
+}
