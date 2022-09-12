29f6f37438e535006c9557a0ed506b3e31e38d3c
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-12 04:04:54 -0700

    two-two

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Print combination of two digit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alp;
+int alpha;
+for (alp = 0; alp <= 100; alp++)
+{
+for (alpha = 0; alpha <= 100; alpha++)
+{	
+if (alpha > alp)
+{
+putchar((alp/10) + 48);
+putchar((alp%10) + 48);
+putchar(44);
+putchar((alpha/10) + 48);
+putchar((alpha%10) + 48);
+if (alp != 98 || alpha != 99)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
