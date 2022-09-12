6d43b3e213c67a972e34c9ee7f30197b780a248d
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-12 02:57:39 -0700

    hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 {
 int alp;
 int alpha;
-for (alp = 48 ; alp <= 57 ; alp++)
+for (alp = 48; alp <= 57; alp++)
 {
-for (alpha = 49; alpha <= 57 ; alpha++)
+for (alpha = 49; alpha <= 57; alpha++)
 if (alpha > alp)
 {		
 putchar(alp);
