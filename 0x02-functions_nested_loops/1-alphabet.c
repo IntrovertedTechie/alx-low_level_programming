ac4d9537d643c68467e6f4925c0bde4c370c6fdc
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-13 11:11:24 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - LowerCase
+ *
+ * Return: Always 0 ( Success)
+ */
+void print_alphabet(void)
+{
+int alp;
+for (alp = 97; alp <= 122; alp++)
+{
+_putchar(alp);
+}
+_putchar(10);
+}
