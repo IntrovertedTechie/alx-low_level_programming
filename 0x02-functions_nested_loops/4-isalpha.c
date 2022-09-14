30005278e1cdee221ad610d0d4923d87e2eb29e7
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-14 07:02:52 -0700

    betty butter oya behave

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,8 @@
 /**
  * _isalpha - checks for alphabetical letters
  * @c : checks for alphabetic character that are lower or upper case.
- * Returns:returns 0 or 1 depending on condition.
+ * Returns: 1 if c is a letter, lowercase or uppercase
+ * Returns: 0 otherwise
  */
 int _isalpha(int c)
 {
