1d8dd81a9aa7fabef67df15ad9fcb6b03dea209b
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-14 07:07:43 -0700

    hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - checks for alphabetical letters
  * @c : checks for alphabetic character that are lower or upper case.
- * Returns: 1 if c is a letter, lowercase or uppercase
+ * Return: return 1 if c is a letter, lowercase or uppercase
  * 0 otherwise
  */
 int _isalpha(int c)
