415392cce3bc84f6d2a094f4f96a5ae57625919e
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-14 07:31:10 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,9 @@ void jack_bauer(void)
 {
 int a;
 int b;
-for(a = 0; a < 24; a++)
+for (a = 0; a < 24; a++)
 {
-for(b =0; b < 60; b++)
+for (b = 0; b < 60; b++)
 {
 if (a < 10)
 {
@@ -36,6 +36,3 @@ _putchar('\n');
 }
 }
 }
-	
-
-
