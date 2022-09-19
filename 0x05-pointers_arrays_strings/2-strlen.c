3fc10da87bf3f86f7f3f1931c341585ddb7d2b56
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-19 06:33:35 -0700

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,11 @@
  * Return: Returns len
  */
 int _strlen(char *s)
-{
-int len;
-for (; *s != '\0'; s++)
-{
-len += 1;
-}
-return (len);
+	{
+	int len;
+	for (; *s != '\0'; s++)
+	{
+  		len = len + 1;
+	}
+	return (len);
 }
