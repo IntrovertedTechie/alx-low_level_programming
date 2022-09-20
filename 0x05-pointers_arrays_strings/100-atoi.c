4be24bab6d402daadffba34a16c2488446daf7fa
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-20 01:47:09 -0700

    convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,11 +16,11 @@ int min = 1;
 int pop = 0;
 while (s[c])
 {
-	if (s[c] == 45)
-	{
- 	min *= -1;
-	}
-	while (s[c] >= 48 && s[c] <= 57)
+if (s[c] == 45)
+{
+ min *= -1;
+}
+while (s[c] >= 48 && s[c] <= 57)
 	{
 		pop = 1;
 		ni = (ni * 10) + (s[c] - '0');
