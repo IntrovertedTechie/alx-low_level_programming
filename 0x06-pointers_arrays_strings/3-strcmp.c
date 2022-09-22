b17b839b853d61ab32bcdb13b82dc40b3dcc87f3
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-22 02:11:13 -0700

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,9 @@
  * @s2: second parameter
  * Return: integer
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int result;
-	
-	result = strcmp(s1, s2);
-	return (result);
+int result;
+result = strcmp(s1, s2);
+return (result);
 }
