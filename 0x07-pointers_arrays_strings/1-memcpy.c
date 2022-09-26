0fb1d3848a1d00be3b97196b608c375c68e4af0d
IntrovertedTechie <zillionaire234@gmail.com>
2022-09-26 12:20:34 -0700

    hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,13 +6,14 @@
  * @dest: is destination memory
  * @src: is source memory
  * @n: number of bytes to be copied
- *  Return:  a string
+ * 
+ * Return:  a string
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
-       	        dest[a] = src[a];
+       	       *dest[a] = *src[a];
 	return (dest);
 }
