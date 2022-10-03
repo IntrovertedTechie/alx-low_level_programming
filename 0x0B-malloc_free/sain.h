a829bf2f970f9025fad31bf8ffab69e09896477f
IntrovertedTechie <zillionaire234@gmail.com>
2022-10-03 08:37:51 -0700

    still trying to solve it

--- 0x0B-malloc_free/sain.h
+++ 0x0B-malloc_free/sain.h
@@ -5,9 +5,9 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void print_grid(int **grid, int width, int height);
 void free_grid(int **grid, int height);
-/*int main(int ac, char *av[]);*/
+int main(int ac, char *av[]);
 char *argstostr(int ac, char **av);
 void print_tab(char **tab);
 char **strtow(char *str);
 int wrdcnt(char *s);
-int _putchar(char c);
+int _putchar(char c)
