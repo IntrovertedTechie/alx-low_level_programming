f6e9ff3f7a1a4c7986eb224d3b3a0b50a6529b69
IntrovertedTechie <zillionaire234@gmail.com>
2022-10-06 12:53:13 -0700

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,4 +7,10 @@ void simple_print_buffer(char *buffer, unsigned int size);
 int *array_range(int min, int max);
 /*void simple_print_buffer(int *buffer, unsigned int size);*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+int main(int argc, char *argv[]);
+int _putchar(char c);
 #endif /* _ALX_A_ */
