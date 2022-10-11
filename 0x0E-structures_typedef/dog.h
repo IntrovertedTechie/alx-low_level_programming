af1543dea6797b7d80d6e05ba3f0f5950b39fc08
IntrovertedTechie <zillionaire234@gmail.com>
2022-10-11 09:52:46 -0700

    README

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_A_
+#define _DOG_A_
+
+/**
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_A_ */
