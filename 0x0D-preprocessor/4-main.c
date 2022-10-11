b87e9f18596b4e359b91dd893a23be0607249f0e
IntrovertedTechie <zillionaire234@gmail.com>
2022-10-11 08:59:10 -0700

    4-sum.h - function-like macro SUM(x, y) that computes the sum of the numbers x and y.

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "4-sum.h"
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_A_
+#define _SUM_A_
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* _SUM_A_ */
