fd5db971e41be08427ef0c470389797932eaaa23
Introvertedtechie <zillionaire234@gmail.com>
2023-04-12 09:26:06 +0000

    Oga

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stddef.h>
+
+/* function that searches for a value in an array of integers using the Linear search algorithm */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
+
