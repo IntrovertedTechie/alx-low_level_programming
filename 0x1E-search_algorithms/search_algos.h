19cff1d8994b67ce82f8ec5844119dbe4e13b98e
Introvertedtechie <zillionaire234@gmail.com>
2023-04-12 09:55:59 +0000

    ds"

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
-
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* function that searches for a value in an array of integers using the Linear search algorithm */
 int linear_search(int *array, size_t size, int value);
